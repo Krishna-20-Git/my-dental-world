9a42bd28a5aaeb23c7ac25d3e0e17d62ac881e97
Krishna <krishnadpsranchi@gmail.com>
2026-01-11 02:06:53 +0530

    Fix: Added missing axios dependency to client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import Admin from "./admin.jsx"; // Make sure filename matches (admin.jsx or Admin.jsx)
+import admin from "./admin.jsx"; // CHECK THIS: Is your file named admin.jsx or Admin.jsx?
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Patient Route (Home) */}
+        {/* The Patient Page */}
         <Route path="/" element={<App />} />
 
-        {/* Doctor Route (Secret Dashboard) */}
+        {/* The Doctor Page */}
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
